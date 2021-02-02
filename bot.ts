6dd7f2ba24a8329cca61e9d1ab93fc7938232073
otallyto <rodrigues.tallyto@hotmail.com>
2021-02-02 18:37:55 -0400

    modify bot.ts

--- bot.ts
+++ bot.ts
@@ -1,7 +1,22 @@
-import { Telegraf } from 'telegraf'
-require('dotenv').config()
-const bot = new Telegraf(process.env.BOT_TOKEN as string)
-console.log("bot version 0.0.0")
-bot.command('start', (ctx) => ctx.reply('Olá é um prazer conversar com você'))
-bot.command('shit', Telegraf.reply('💩'))
-bot.launch()
+import { Telegraf, session } from 'telegraf';
+
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN as string);
+console.log('bot version 0.0.0');
+bot.command('start', (ctx) => ctx.reply(`Olá ${ctx.chat.first_name} é um prazer conversar com você`));
+bot.command('shit', Telegraf.reply('💩'));
+bot.command('gabriel', Telegraf.reply('Vai estudar JavaScript'));
+
+bot.on('text', (ctx) => {
+  // Explicit usage
+  // ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
+
+  // Using context shortcut
+  ctx.reply(ctx.chat);
+});
+
+bot.launch();
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
